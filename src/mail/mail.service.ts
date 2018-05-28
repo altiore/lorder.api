8aaa6856eb81f3725d238843c9fca91605bcbcd4
Razzwan <razvanlomov@gmail.com>
2018-05-28 21:56:41 +0300

    feat(mail): fix path to templates form production

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,22 +10,34 @@ import { User } from '../@entities/user';
 
 @Injectable()
 export class MailService {
-  constructor() {}
+  constructor() {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  }
+
+  private static readonly INVITE_TEMPLATE = 'invite';
 
   /**
    * using SendGrid's v3 Node.js Library
    * @see https://github.com/sendgrid/sendgrid-nodejs
    */
-  private async send(msg: MessageDto): Promise<[ClientResponse, {}]> {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    return await sgMail.send(msg);
+  private send(msg: MessageDto): Promise<[ClientResponse, {}]> {
+    return sgMail.send(msg);
+  }
+
+  /**
+   * Render template: @see https://mjml.io/
+   * Put variables to template: @see https://github.com/janl/mustache.js
+   */
+  private putParamsToTemplate(template: 'invite', params: object): string {
+    return Mustache.render(readFileSync(resolve(process.env.MAILS_PATH, `${template}.html`), 'utf8'), params);
   }
 
   public sendInvite(user: User): Promise<[ClientResponse, {}]> {
-    const output = Mustache.render(readFileSync(resolve(process.env.pwd, 'mails/invite.html'), 'utf8'), {
+    const output = this.putParamsToTemplate(MailService.INVITE_TEMPLATE, {
       LINK: 'https://google.com/test/link',
       LINK_NAME: 'Button Name',
     });
+
     return this.send({
       from: 'altiore@gmail.com',
       to: user.email,
