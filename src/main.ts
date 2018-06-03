4c854fc6062bdf486bfcb0070153858600ba368e
Razzwan <razvanlomov@gmail.com>
2018-06-03 17:11:25 +0300

    add base path and global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,25 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from './@common/pipes/validation.pipe';
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || process.env.HOSTNAME || 'localhost';
+const SCHEMA = process.env.NODE_ENV === 'production' ? 'https' : 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('v1');
 
   const options = new DocumentBuilder()
     .setTitle('Altiore')
     .setDescription('Altiore API documentation')
     .setVersion('1.0')
     .addBearerAuth('Authorization', 'header')
+    .setSchemes(SCHEMA)
+    .setBasePath('v1')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  await app.listen(PORT, () => console.log(`Listening on ${SCHEMA}://${HOST}:${PORT}`));
 }
 bootstrap();
