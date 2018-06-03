a6d3bf53a85ccfbb58f8fdb16a2f1449d25cb910
Razzwan <razvanlomov@gmail.com>
2018-06-03 15:56:40 +0300

    config add ssl param

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { APP_FILTER } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 import { AuthModule } from './auth/auth.module';
 import { ProjectModule } from './project/project.module';
@@ -8,7 +9,9 @@ import { UserModule } from './user/user.module';
 import { AllExceptionsFilter } from './@common/filters/all-exceptions.filter';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot(), UserModule, ProjectModule],
+  imports: [AuthModule, TypeOrmModule.forRoot({
+    ssl: { sslmode: 'require' },
+  }), UserModule, ProjectModule],
   providers: [
     {
       provide: APP_FILTER,
