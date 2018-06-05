acbbafc8e16b1bba6fd6213ff75350b41cc87ad8
Razzwan <razvanlomov@gmail.com>
2018-06-06 00:28:57 +0300

    feat(codecov): add codecov support

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/src/.*(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+  ],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.d.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  coverageDirectory: './coverage/',
+  coverageReporters: [
+    'json',
+    'lcov',
+    'text-summary',
+  ],
+};
