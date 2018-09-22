818cad21bf4f50e21610c9fead2f6b3557276578
Razzwan <razvanlomov@gmail.com>
2018-09-22 12:46:12 +0300

    feat(temp): logs on

--- tests/users/@fixtures/users.ts
+++ tests/users/@fixtures/users.ts
@@ -1,16 +1,20 @@
+import { ROLES } from '../../../src/@orm/role';
 import { createUsers } from '../../@fixtureCreators';
 
 export const usersFixture = createUsers([
   {
     email: 'user@mail.com',
-    roles: [{ name: 'user' }],
+    roles: [{ name: ROLES.USER }],
+    status: 10,
   },
   {
     email: 'admin@mail.com',
-    roles: [{ name: 'user' }, { name: 'admin' }],
+    roles: [{ name: ROLES.USER }, { name: ROLES.ADMIN }],
+    status: 10,
   },
   {
     email: 'super-admin@mail.com',
-    roles: [{ name: 'user' }, { name: 'admin' }, { name: 'super-admin' }],
+    roles: [{ name: ROLES.USER }, { name: ROLES.ADMIN }, { name: ROLES.SUPER_ADMIN }],
+    status: 10,
   },
 ]);
