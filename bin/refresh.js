d78a2db8dc44f446e2fcc89b524417162087f664
Razzwan <razvanlomov@gmail.com>
2018-09-22 11:21:18 +0300

    fix(refresh): fix grant all to correct user

--- bin/refresh.js
+++ bin/refresh.js
@@ -2,13 +2,6 @@ const { Client } = require("pg");
 
 require("dotenv").config();
 
-console.log("before create Client", {
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  user: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-});
 const client = new Client({
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
@@ -16,9 +9,15 @@ const client = new Client({
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
 });
-console.log("after create Client");
 
 if (!client || !client.connect) {
+  console.log("connection not created", {
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    user: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+  });
   process.exit(1);
 }
 
@@ -27,12 +26,11 @@ client.connect(err => {
     console.log(err);
     process.exit(1);
   }
-  console.log("after connection");
   client.query(
     `
   DROP SCHEMA public CASCADE;
   CREATE SCHEMA public;
-  GRANT ALL ON SCHEMA public TO postgres;
+  GRANT ALL ON SCHEMA public TO ${process.env.TYPEORM_USERNAME};
   GRANT ALL ON SCHEMA public TO public;
   COMMENT ON SCHEMA public IS 'standard public schema';
 `,
@@ -42,7 +40,6 @@ client.connect(err => {
         err && console.log(`\x1b[33m${err}\x1b[33m`);
         process.exit(1);
       }
-      console.log("result of query is", res);
       client.end();
     }
   );
