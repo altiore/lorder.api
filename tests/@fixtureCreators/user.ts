6801d871e80d37bbb897ff6136b274edfb4d34ff
Razzwan <razvanlomov@gmail.com>
2018-09-25 13:33:58 +0300

    fix(tests): use helper from typeorm-fixtures for relation

--- tests/@fixtureCreators/user.ts
+++ tests/@fixtureCreators/user.ts
@@ -1,16 +1,13 @@
-import { intersectionWith, isMatch } from 'lodash';
 import { fixtureCreator, relation } from 'typeorm-fixtures';
 
 import { Role } from '../../src/@orm/role';
 import { User } from '../../src/@orm/user';
 
 export const createUsers = fixtureCreator<User>(User, function(entity, index) {
-  const roles = entity.roles;
   return {
     email: `test${index}@mail.com`,
     status: 10,
     ...entity,
-    // roles: relation(this, Role, entity.roles),
-    roles: (this.Role && roles ? intersectionWith(this.Role, roles, isMatch) : this.Role || []) as Role[],
+    roles: relation(this, Role, entity.roles),
   };
 });
