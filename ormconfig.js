1937d4c8f421dd8876e39ffb51622a7f471088d2
Razzwan <razvanlomov@gmail.com>
2018-10-14 23:14:13 +0300

    fix(chore): for run migrations

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,14 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
-const baseDir = process.env.BASE_DIR || "dist";
-const scriptExt = baseDir === "dist" ? "js" : "ts";
+const baseDir = process.env.BASE_DIR || 'dist';
+const scriptExt = baseDir === 'dist' ? 'js' : 'ts';
 
 module.exports = {
-  type: process.env.TYPEORM_CONNECTION || "postgres",
+  type: process.env.TYPEORM_CONNECTION || 'postgres',
 
-  host: process.env.TYPEORM_HOST || "localhost",
+  host: process.env.TYPEORM_HOST || 'localhost',
   port: process.env.TYPEORM_PORT || 5432,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
@@ -17,8 +17,8 @@ module.exports = {
   entities: [`${baseDir}/@orm/**/**.entity.${scriptExt}`],
   migrations: [`${baseDir}/@orm/@migrations/*.${scriptExt}`],
 
-  synchronize: false,
-  logging: process.env.TYPEORM_LOGGING === "true",
+  synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+  logging: process.env.TYPEORM_LOGGING === 'true',
 
   cli: {
     migrationsDir: `${baseDir}/@orm/@migrations`,
