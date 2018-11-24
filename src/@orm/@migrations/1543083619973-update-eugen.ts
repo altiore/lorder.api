286e3b4f71cd7aabf6b861dcfed51f80b535c5e5
Razzwan <razvanlomov@gmail.com>
2018-11-24 20:31:57 +0200

    feat(status): set 4 status to eugen

---
+++ src/@orm/@migrations/1543083619973-update-eugen.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+const email = 'lider640@mail.ru';
+const projectTitle = 'Free World';
+
+export class new1543083619973 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    const [user] = await queryRunner.query(`SELECT "id" from "user" where "email"='${email}'`);
+    const [project] = await queryRunner.query(`SELECT "id" from "project" where "title"='${projectTitle}'`);
+    if (user && project) {
+      await queryRunner.query(
+        `UPDATE "user_project" SET "accessLevel"=4 where "projectId"='${project.id}' AND "memberId"='${user.id}'`
+      );
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    const [user] = await queryRunner.query(`SELECT "id" from "user" where "email"='${email}'`);
+    const [project] = await queryRunner.query(`SELECT "id" from "project" where "title"='${projectTitle}'`);
+    if (user && project) {
+      await queryRunner.query(
+        `UPDATE "user_project" SET "accessLevel"=1 where "projectId"='${project.id}' AND "memberId"='${user.id}'`
+      );
+    }
+  }
+}
