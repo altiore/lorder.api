ee69da59d3c026714be39e33ed6b1111432ce1f1
Razzwan <razvanlomov@gmail.com>
2019-08-02 10:02:34 +0300

    fix preinstall script

--- bin/gcpSetup.js
+++ bin/gcpSetup.js
@@ -1,2 +1,13 @@
 var fs = require('fs');
-fs.writeFile(process.env.GOOGLE_APPLICATION_CREDENTIALS, process.env.GOOGLE_CLOUD_CRED, err => {});
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS && process.env.GOOGLE_CLOUD_CRED) {
+  fs.writeFile(
+    process.cwd() + '/' + process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    process.env.GOOGLE_CLOUD_CRED,
+    err => {}
+  );
+} else {
+  console.error(
+    'Environment variables GOOGLE_APPLICATION_CREDENTIALS and GOOGLE_CLOUD_CRED was not set'
+  );
+  process.exit(1);
+}
