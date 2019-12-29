fef27f6ae7567b68eb02523da3c73f9b406f0a0d
Razzwan <razvanlomov@gmail.com>
2019-12-29 14:54:58 +0200

    other variant of CookieMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AllExceptionsFilter } from './@common/filters/all-exceptions.filter';
-import { CookieMiddleware } from './@common/middlewares/cookie.middleware';
 import { ValidationPipe } from './@common/pipes/validation.pipe';
 import { AuthModule } from './auth/auth.module';
 import { FeedbackModule } from './feedback/feedback.module';
@@ -45,8 +44,4 @@ import { UserModule } from './user/user.module';
     },
   ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): any {
-    consumer.apply(CookieMiddleware);
-  }
-}
+export class AppModule {}
