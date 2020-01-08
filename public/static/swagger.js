2e2042e098f9dd9ddecbe3065944af38644b8d7b
Razzwan <razvanlomov@gmail.com>
2020-01-08 20:37:53 +0200

    Добавил сохранение ключа в рамках сессии

--- public/static/swagger.js
+++ public/static/swagger.js
@@ -1,28 +1,42 @@
 (function() {
+  const TOKEN_NAME = 'JwtToken';
   setTimeout(function() {
-    const openModalCollection = document.getElementsByClassName('btn authorize unlocked');
-    const openBtn = openModalCollection.item(0);
-    if (openBtn) {
-      openBtn.addEventListener('click', function() {
-        setTimeout(function() {
-          const authCollection = document.getElementsByClassName(
-            'btn modal-btn auth authorize button'
-          );
-          const authBtn = authCollection.item(0);
-          authBtn.addEventListener('click', function(e) {
-            console.log('click to AUTH button!!!');
-          });
-        }, 1000);
-        console.log('Button was clicked!!!');
+    // 1. If token already exists, set it from sessionStorage
+    const JWTToken = sessionStorage.getItem(TOKEN_NAME);
+    if (JWTToken) {
+      ui.authActions.preAuthorizeImplicit({
+        auth: {
+          schema: {
+            type: 'apiKey',
+            in: 'header',
+            name: 'Authorication',
+            get: function(key) {
+              return this[key];
+            },
+          },
+          name: 'bearer',
+          value: JWTToken,
+        },
+        token: {},
+        isValid: true,
       });
     }
 
-    const ui = SwaggerUIBundle({});
-    console.log('SwaggerUIBundle', {
-      SwaggerUIBundle,
-      ui,
-      uiProps: Object.getOwnPropertyNames(ui),
-      uiState: ui.getState(),
-    });
+    // 2. If token was not set yet, save it to sessionStorage
+    const openBtn = document.querySelector('btn authorize unlocked');
+    if (openBtn) {
+      openBtn.addEventListener('click', function() {
+        setTimeout(function() {
+          const authBtn = document.querySelector('btn modal-btn auth authorize button');
+          authBtn.addEventListener('click', function(e) {
+            const tokenInput = document.querySelector('input:not([class])');
+            if (tokenInput && tokenInput.value) {
+              sessionStorage.setItem(TOKEN_NAME, tokenInput.value);
+              console.log('Token vas successfully saved!!!');
+            }
+          });
+        }, 1000);
+      });
+    }
   }, 1000);
 })();
