168766dc75fe781afc13aa000b34dc8d22cc9b3b
Razzwan <razvanlomov@gmail.com>
2020-02-16 21:28:10 +0200

    filter tasks from default project. We do not need them

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -21,7 +21,7 @@ export class TaskService {
 
   public async findAll(pagesDto: TaskPagination, user: User): Promise<Task[]> {
     const userProjects = await this.projectService.findAllParticipantByUser({}, user, ACCESS_LEVEL.RED);
-    const projectIds = userProjects.map(el => el.project.id);
+    const projectIds = userProjects.map(el => el.project.id).filter(id => id !== user.defaultProjectId);
     if (!projectIds.length) {
       return [];
     }
